6f274d1a3fb638296796c7fb054feae7c51b62e4
Mohamed Rezq <93948617+mohamed22311@users.noreply.github.com>
2025-03-09 00:29:30 +0200

    Update TransformerEncoderBlock.py

--- model/TransformerEncoderBlock.py
+++ model/TransformerEncoderBlock.py
@@ -1,6 +1,6 @@
 from torch import nn
-from MultiHeadSelfAttention import MultiHeadSelfAttention
-from MLPBlock import MLPBlock
+from .MultiHeadSelfAttention import MultiHeadSelfAttention
+from .MLPBlock import MLPBlock
 
 class TransformerEncoderBlock(nn.Module):
     def __init__(self, embed_dim:int=768, num_heads:int=12, ff_dim:int=3072, dropout:float=0.1, atten_dropout:float=0.0)->None:
